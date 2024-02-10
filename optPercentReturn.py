2abe794411326beb095c05125825616b6008eaeb
Lucas-Luwa <lluwa3@gatech.edu>
2024-02-10 02:22:19 -0500

    Calculate Percent Returns

---
+++ optPercentReturn.py
@@ -0,0 +1,18 @@
+import math
+
+def sigmoid(x):
+    return float(1/(1 + math.exp(-x)))
+
+def calcPercentReturn(type, strike, closingPrice, premium):
+    if type == 'Call':
+        rawReturn = closingPrice - (strike + premium)
+    elif type == 'Put':
+        rawReturn = (strike - premium) - closingPrice
+    else:
+        return ValueError("Must use 'Call' or 'Put' option type")
+
+    percent = round(float(rawReturn/premium) * 100, 2)
+    return percent
+
+#Testing
+print(calcPercentReturn('Call', 2, 5, 1))
