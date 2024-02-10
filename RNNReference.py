d6b5cc17760f54ead4ff40e96c83d78a8392f77d
Lucas-Luwa <lluwa3@gatech.edu>
2024-02-09 22:17:42 -0500

    Create RNNReference.py

---
+++ RNNReference.py
@@ -0,0 +1,25 @@
+import torch.nn as nn
+import torch
+
+class RNNReference(nn.Module):
+    def __init__(self, input_size, hidden_size, output_size):
+        super(RNNReference, self).__init__()
+
+        self.hidden_size = hidden_size
+
+        self.i2h = nn.Linear(input_size + hidden_size, hidden_size)
+        self.h2o = nn.Linear(hidden_size, output_size)
+        self.softmax = nn.LogSoftmax(dim=1)
+
+    def forward(self, input, hidden):
+        combined = torch.cat((input, hidden), 1)
+        hidden = self.i2h(combined)
+        output = self.h2o(hidden)
+        output = self.softmax(output)
+        return output, hidden
+
+    def initHidden(self):
+        return torch.zeros(1, self.hidden_size)
+
+n_hidden = 128
+rnn = RNNReference(7, n_hidden, 5)
